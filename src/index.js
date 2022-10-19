ad0b204dab227aa9ae56e3393b981b766af38f94
brunosoares99 <bruno_soares_vieira@outlook.com>
2022-10-19 20:39:08 -0300

    feat: final landing page test

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 const menuButton = document.querySelector('.header__menu-open');
 const ulMenu = document.querySelector('.header__menu-ul');
 const menuClose = document.querySelector('.header__menu-close');
-
+const dropbtn = document.querySelector('.dropbtn');
+const optionsDropDownMenuLanguage = document.querySelectorAll('.dropdown__options');
+const searchButton = document.querySelector('.header__search-icon');
+const boxSearch = document.querySelector('.header__box-search');
+const body = document.querySelector('body');
 
 const handleMenu = () => {
     if(ulMenu.classList.contains('header__menu-ul--active')) {
         ulMenu.classList.remove('header__menu-ul--active');
         menuClose.classList.remove('header__menu-close--active');
+        body.classList.remove('body__fixed');
         return
     }
     ulMenu.classList.add('header__menu-ul--active');
     menuClose.classList.add('header__menu-close--active');
+    body.classList.add('body__fixed');
 }
-
-menuButton.addEventListener('click', handleMenu);
-menuClose.addEventListener('click', handleMenu);
-
 
 const horizontalScroll = () => {
     const wrapper = document.querySelector('.cases__slider-wrapper');
@@ -29,4 +31,40 @@ const horizontalScroll = () => {
     })
 }
 
+const dropDownMenuLanguage = () => {
+    document.getElementById("dropDownLanguage").classList.toggle("show");
+}
+
+const handleDropDown = (event) => {
+    if (event.target.matches('.dropbtn')) return
+    const dropdowns = document.querySelectorAll(".dropdown-content");
+    console.log(dropdowns)
+    dropdowns.forEach(dropdown => {
+        console.log(dropdown)
+        if (dropdown.classList.contains('show')) {
+            dropdown.classList.remove('show');
+        }
+    })
+}
+
+optionsDropDownMenuLanguage.forEach(option => {
+    option.addEventListener('click', (event) => {
+        dropbtn.innerHTML = event.target.innerHTML;
+        handleDropDown(event);
+    });
+})
+
+handleSearch = () => {
+    const handleClass = 'header__box-search--active'
+    if(boxSearch.classList.contains(handleClass)) {
+        boxSearch.classList.remove(handleClass);
+        return
+    }
+    boxSearch.classList.add(handleClass);
+}
+
+menuButton.addEventListener('click', handleMenu);
+menuClose.addEventListener('click', handleMenu);
 horizontalScroll()
+dropbtn.addEventListener('click', handleDropDown);
+searchButton.addEventListener('click', handleSearch);
